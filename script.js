40f1b4433151a50c0bceb6c912810770f2d7e455
mind matters <121178335+4-Akshat-7@users.noreply.github.com>
2023-11-08 23:09:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 function analyzeMood() {
-
     var sleep = parseFloat(document.getElementById("sleep").value);
     var exercise = parseFloat(document.getElementById("exercise").value);
     var nutrition = parseFloat(document.getElementById("nutrition").value);
-
 
     if (isNaN(sleep) || isNaN(exercise) || isNaN(nutrition)) {
         alert("Please enter valid numeric values for sleep, exercise, and nutrition.");
@@ -11,6 +9,9 @@ function analyzeMood() {
     }
 
     var mood = 2.8 + 0.2 * sleep + 0.175 * exercise + 0.00175 * nutrition;
+
+    // Limit mood to a maximum value of 10
+    mood = Math.min(mood, 10);
 
     document.getElementById("predictedMood").innerText = mood.toFixed(2);
     document.getElementById("result").style.display = "block";
